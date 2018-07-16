e6f898289dc46a359820fe965495a68fd983bc61
maxjeffwell <maxjeffwell@gmail.com>
2018-07-16 15:09:30 -0400

    added yearOfBirth

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // Refactored to function expression
 // Added some parameters to be passed in the function
 const whoAmI = (name, age) => {
-  // Refactored code to use string interpolation
-console.log(`Hi, my name is ${name} and Im ${age} years old`);
-}
+  const yearOfBirth = 2018 - age;
 
+console.log(`Hi, my name is ${name} and Im ${age} years old`);
+console.log(`I was born in ${yearOfBirth}`);
+}
 whoAmI("Antoine", 28);
+whoAmI("Jeff", 30);
