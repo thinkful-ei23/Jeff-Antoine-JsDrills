c009b9eb35a2ba3b7823a273ed5384fa5054ecc4
Antoine Catt <antoinecatt@gmail.com>
2018-07-16 14:25:17 -0700

    commented beyond function

--- function-drills-2.js
+++ function-drills-2.js
@@ -7,21 +7,21 @@
 // }
 // console.log(jediName('Beyonce', 'Knowles'));
 
-const beyond = num => {
-  if(num === Infinity) {
-    console.log("And beyond!");
-  } else if(isFinite(num) && num > 0) {
-    console.log("To infinity");
-  } else if(isFinite(num) && num < 0) {
-    console.log("To negative infinity");
-  } else {
-    console.log("Staying home");
-  }
+// const beyond = num => {
+//   if(num === Infinity) {
+//     console.log("And beyond!");
+//   } else if(isFinite(num) && num > 0) {
+//     console.log("To infinity");
+//   } else if(isFinite(num) && num < 0) {
+//     console.log("To negative infinity");
+//   } else {
+//     console.log("Staying home");
+//   }
 
-}
+// }
 
-beyond(1);
-beyond(-1);
-beyond(0);
-beyond(Infinity);
+// beyond(1);
+// beyond(-1);
+// beyond(0);
+// beyond(Infinity);
 
