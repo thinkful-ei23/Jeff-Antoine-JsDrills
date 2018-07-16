a59e845648a5a4e6bfcfc1e0b3001a9796614cfb
Antoine Catt <antoinecatt@gmail.com>
2018-07-16 13:08:21 -0700

    added check for name and age

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 const yearOfBirth = age => {
   if (age < 0) {
-    throw new Error("Age cannot be negative");
+    throw new Error("Age cannot be negative");;
   };
   return 2018 - age;
 }
   
-  
-
-
-
-
 const whoAmI = (name, age) => {
  
   try {
     const yob = yearOfBirth(age)
+    if(name === undefined || age === undefined) {
+      console.error("Arguments not valid");
+    } else {
     console.log(`Hi, my name is ${name} and Im ${age} years old`);
     console.log(`I was born in ${yob}`);
-  } catch (e) {
+  }
+  } catch(e) {
       console.error(e.message);
   }
+
 };
 
 whoAmI("Antoine", 28);
 whoAmI("Jeff", 30);
 whoAmI("Jeff", -1);
+
+
+
