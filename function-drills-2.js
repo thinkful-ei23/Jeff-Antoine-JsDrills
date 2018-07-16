46887e57a211f3ed135d48740ca9579632788616
Antoine Catt <antoinecatt@gmail.com>
2018-07-16 14:20:21 -0700

    added beyond function

--- function-drills-2.js
+++ function-drills-2.js
@@ -1,8 +1,27 @@
-const jediName = (firstName, lastName) => {
-  let jediFirst = firstName.slice(0, 2);
-  let jediLast = lastName.slice(0, 3);
-  return jediLast.concat(jediFirst);
+// const jediName = (firstName, lastName) => {
+//   let jediFirst = firstName.slice(0, 2);
+//   let jediLast = lastName.slice(0, 3);
+//   return jediLast.concat(jediFirst);
   
 
+// }
+// console.log(jediName('Beyonce', 'Knowles'));
+
+const beyond = num => {
+  if(num === Infinity) {
+    console.log("And beyond!");
+  } else if(isFinite(num) && num > 0) {
+    console.log("To infinity");
+  } else if(isFinite(num) && num < 0) {
+    console.log("To negative infinity");
+  } else {
+    console.log("Staying home");
+  }
+
 }
-console.log(jediName('Beyonce', 'Knowles'));
+
+beyond(1);
+beyond(-1);
+beyond(0);
+beyond(Infinity);
+
